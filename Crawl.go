31fcf0561b3ab9d52b8df047ffc5972e92c76522
Vu Viet Duy <vuvietduy1234@gmail.com>
2023-06-25 23:23:28 +0700

    Jun 25, 2023

--- Crawl.go
+++ Crawl.go
@@ -4,36 +4,74 @@ import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"regexp"
+	"sync"
 )
 
-func CrawlUrl(link string) string {
+func crawlWeb(url string) string {
 	client := resty.New()
-
-	resp, err := client.R().Get(link)
+	resp, err := client.R().Get(url)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 	}
 
-	if resp.StatusCode() != 200 {
-		fmt.Println("status", resp.Status())
-	}
+	body := resp.Body()
+	html := string(body)
 
-	res := string(resp.Body())
-	fmt.Println(res)
-	return res
+	fmt.Println("crawl successful")
+	return html
+	//fmt.Print(html)
 }
 
-func findLink(html string) []string {
-	//links := make([]string, 0)
-	pattern := `href="([^"]*)"`
-
+func extractLinks(html string) []string {
+	pattern := `href=["'](.*?)["']`
 	re := regexp.MustCompile(pattern)
+
 	matches := re.FindAllString(html, -1)
-	fmt.Println(matches)
+
 	return matches
 }
 
 func Crawl() {
-	html := CrawlUrl("https://gobyexample.com/")
-	findLink(html)
+	url := "https://gobyexample.com/"
+
+	html := crawlWeb(url)
+
+	links := extractLinks(html)
+
+	var mutex sync.Mutex
+
+	//for _, link := range links {
+	//	fmt.Println(link)
+	//}
+
+	//ch := make(chan string)
+
+	var wg = sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				mutex.Lock()
+
+				if len(links) == 0 {
+					mutex.Unlock()
+					return
+				}
+
+				link := links[0]
+				links = links[1:]
+				mutex.Unlock()
+
+				fmt.Println(link)
+				//crawlWeb(link)
+			}
+		}()
+	}
+
+	//for _, link := range links {
+	//	ch <- link
+	//}
+	wg.Wait()
 }
